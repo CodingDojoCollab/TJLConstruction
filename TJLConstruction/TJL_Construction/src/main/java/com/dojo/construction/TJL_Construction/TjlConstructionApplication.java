5b5afc9d2d682d9f050fbe0750126897baf8999d
James Turcotte <77208650+JamesTurcotte01@users.noreply.github.com>
2021-04-26 14:35:09 -0400

    Add files via upload

---
+++ TJLConstruction/TJL_Construction/src/main/java/com/dojo/construction/TJL_Construction/TjlConstructionApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.construction.TJL_Construction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjlConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjlConstructionApplication.class, args);
+	}
+
+}
